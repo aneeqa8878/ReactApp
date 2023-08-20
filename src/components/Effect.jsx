11eb96f24ac3b97160837a1906d453750f3dcd2e
unknown <adiljutt116@gmail.com>
2023-08-20 19:48:18 +0500

    New file1

--- src/components/Effect.jsx
+++ src/components/Effect.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Loader from './Loader'
 
 
 function Effect() {
   return (
     <div>
-    
+
       <h1>Effect</h1>
     </div>
   )
---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Loader.css"
+
+
+function Loader() {
+  return (
+    <div>
+      <div class="loader"></div>
+    </div>
+  )
+}
+
+export default Loader
